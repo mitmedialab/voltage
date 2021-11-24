c257c399ad50f607cff8157b57009bdf31048110
Farhan - Media <farhan@fixstars.com>
2021-11-24 15:43:45 -0500

    Add original training scripts

---
+++ train/train_spatial_original.py
@@ -0,0 +1,315 @@
+import os
+import sys
+import random
+from multiprocessing import Pool
+import matplotlib
+
+import matplotlib.pyplot as plt
+import numpy as np
+from numpy import genfromtxt
+import time
+from nd2reader import ND2Reader
+from collections import Counter
+import tifffile as tiff
+import cv2
+import pickle
+import glob
+import json
+from scipy.stats import pearsonr
+from sklearn.cluster import AgglomerativeClustering
+
+from read_roi import read_roi_zip, read_roi_file
+import matplotlib.pyplot as plt
+import tifffile as tiff
+import time
+import gc
+import numpy as np
+import cv2
+from datetime import datetime
+import itertools
+
+
+import pandas as pd
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import accuracy_score
+from sklearn import svm
+from sklearn.decomposition import PCA
+from sklearn.naive_bayes import GaussianNB
+from sklearn.linear_model import Perceptron
+from sklearn.neural_network import MLPClassifier
+from sklearn.metrics import classification_report,confusion_matrix
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import f1_score
+from sklearn.datasets import make_classification
+from sklearn.metrics import roc_auc_score
+from sklearn.model_selection import GridSearchCV
+from sklearn.tree import DecisionTreeClassifier, export_graphviz
+from sklearn.feature_selection import RFE
+from sklearn.linear_model import LogisticRegression
+from sklearn import metrics
+from sklearn.metrics import roc_curve, auc
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.feature_selection import SelectKBest, f_classif
+from sklearn.linear_model import SGDClassifier
+from sklearn.utils import shuffle
+
+import keras
+from keras.models import *
+from keras.models import load_model
+from keras.layers import *
+from keras.optimizers import *
+from keras.callbacks import ModelCheckpoint, LearningRateScheduler
+from keras.losses import binary_crossentropy
+import keras.backend as K
+from sklearn.model_selection import train_test_split
+from keras.callbacks import EarlyStopping, ReduceLROnPlateau, ModelCheckpoint, TensorBoard
+from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array, load_img
+import tensorflow as tf
+
+
+if(int(tf.__version__[0]) > 1):
+    gpus = tf.config.experimental.list_physical_devices('GPU')
+    if gpus:
+        try:
+            for gpu in gpus:
+                tf.config.experimental.set_memory_growth(gpu, True)
+        except RuntimeError as e:
+            print(e)
+else:
+    session_config = tf.ConfigProto(gpu_options=tf.GPUOptions(allow_growth=True))
+    tf.Session(config = session_config)
+
+def dice_coeff(y_true, y_pred):
+    smooth = 1.
+    y_true_f = K.flatten(y_true)
+    y_pred_f = K.flatten(y_pred)
+    intersection = K.sum(y_true_f * y_pred_f)
+    score = (2. * intersection + smooth) / (K.sum(y_true_f) + K.sum(y_pred_f) + smooth)
+    return score
+
+
+def dice_loss(y_true, y_pred):
+    loss = 1 - dice_coeff(y_true, y_pred)
+    return loss
+
+
+def bce_dice_loss(y_true, y_pred):
+    loss = binary_crossentropy(y_true, y_pred) + dice_loss(y_true, y_pred)
+    return loss
+
+
+def weighted_dice_coeff(y_true, y_pred, weight):
+    smooth = 1.
+    w, m1, m2 = weight * weight, y_true, y_pred
+    intersection = (m1 * m2)
+    score = (2. * K.sum(w * intersection) + smooth) / (K.sum(w * m1) + K.sum(w * m2) + smooth)
+    return score
+
+
+def weighted_dice_loss(y_true, y_pred):
+    y_true = K.cast(y_true, 'float32')
+    y_pred = K.cast(y_pred, 'float32')
+    # if we want to get same size of output, kernel size must be odd number
+    if K.int_shape(y_pred)[1] == 128:
+        kernel_size = 11
+    elif K.int_shape(y_pred)[1] == 256:
+        kernel_size = 21
+    elif K.int_shape(y_pred)[1] == 512:
+        kernel_size = 21
+    elif K.int_shape(y_pred)[1] == 1024:
+        kernel_size = 41
+    else:
+        raise ValueError('Unexpected image size')
+    averaged_mask = K.pool2d(
+        y_true, pool_size=(kernel_size, kernel_size), strides=(1, 1), padding='same', pool_mode='avg')
+    border = K.cast(K.greater(averaged_mask, 0.005), 'float32') * K.cast(K.less(averaged_mask, 0.995), 'float32')
+    weight = K.ones_like(averaged_mask)
+    w0 = K.sum(weight)
+    weight += border * 2
+    w1 = K.sum(weight)
+    weight *= (w0 / w1)
+    loss = 1 - weighted_dice_coeff(y_true, y_pred, weight)
+    return loss
+
+
+def weighted_bce_loss(y_true, y_pred, weight):
+    # avoiding overflow
+    epsilon = 1e-7
+    y_pred = K.clip(y_pred, epsilon, 1. - epsilon)
+    logit_y_pred = K.log(y_pred / (1. - y_pred))
+
+    # https://www.tensorflow.org/api_docs/python/tf/nn/weighted_cross_entropy_with_logits
+    loss = (1. - y_true) * logit_y_pred + (1. + (weight - 1.) * y_true) * \
+                                          (K.log(1. + K.exp(-K.abs(logit_y_pred))) + K.maximum(-logit_y_pred, 0.))
+    return K.sum(loss) / K.sum(weight)
+
+
+def weighted_bce_dice_loss(y_true, y_pred):
+    y_true = K.cast(y_true, 'float32')
+    y_pred = K.cast(y_pred, 'float32')
+    # if we want to get same size of output, kernel size must be odd number
+    if K.int_shape(y_pred)[1] == 128:
+        kernel_size = 11
+    elif K.int_shape(y_pred)[1] == 256:
+        kernel_size = 21
+    elif K.int_shape(y_pred)[1] == 512:
+        kernel_size = 21
+    elif K.int_shape(y_pred)[1] == 1024:
+        kernel_size = 41
+    else:
+        raise ValueError('Unexpected image size')
+    averaged_mask = K.pool2d(
+        y_true, pool_size=(kernel_size, kernel_size), strides=(1, 1), padding='same', pool_mode='avg')
+    border = K.cast(K.greater(averaged_mask, 0.005), 'float32') * K.cast(K.less(averaged_mask, 0.995), 'float32')
+    weight = K.ones_like(averaged_mask)
+    w0 = K.sum(weight)
+    weight += border * 2
+    w1 = K.sum(weight)
+    weight *= (w0 / w1)
+    loss = weighted_bce_loss(y_true, y_pred, weight) + (1 - weighted_dice_coeff(y_true, y_pred, weight))
+    return loss
+
+
+
+input_size = (64,64,1)
+
+inputs = Input(input_size)
+down1 = Conv2D(64, (3, 3), padding='same')(inputs)
+down1 = BatchNormalization()(down1)
+down1 = Activation('relu')(down1)
+down1 = Conv2D(64, (3, 3), padding='same')(down1)
+down1 = BatchNormalization()(down1)
+down1 = Activation('relu')(down1)
+down1_pool = MaxPooling2D((2, 2), strides=(2, 2))(down1)
+# 64
+
+down2 = Conv2D(128, (3, 3), padding='same')(down1_pool)
+down2 = BatchNormalization()(down2)
+down2 = Activation('relu')(down2)
+down2 = Conv2D(128, (3, 3), padding='same')(down2)
+down2 = BatchNormalization()(down2)
+down2 = Activation('relu')(down2)
+down2_pool = MaxPooling2D((2, 2), strides=(2, 2))(down2)
+# 32
+
+down3 = Conv2D(256, (3, 3), padding='same')(down2_pool)
+down3 = BatchNormalization()(down3)
+down3 = Activation('relu')(down3)
+down3 = Conv2D(256, (3, 3), padding='same')(down3)
+down3 = BatchNormalization()(down3)
+down3 = Activation('relu')(down3)
+down3_pool = MaxPooling2D((2, 2), strides=(2, 2))(down3)
+# 16
+
+down4 = Conv2D(512, (3, 3), padding='same')(down3_pool)
+down4 = BatchNormalization()(down4)
+down4 = Activation('relu')(down4)
+down4 = Conv2D(512, (3, 3), padding='same')(down4)
+down4 = BatchNormalization()(down4)
+down4 = Activation('relu')(down4)
+down4_pool = MaxPooling2D((2, 2), strides=(2, 2))(down4)
+# 8
+
+center = Conv2D(1024, (3, 3), padding='same')(down4_pool)
+center = BatchNormalization()(center)
+center = Activation('relu')(center)
+center = Conv2D(1024, (3, 3), padding='same')(center)
+center = BatchNormalization()(center)
+center = Activation('relu')(center)
+# center
+
+up4 = UpSampling2D((2, 2))(center)
+up4 = concatenate([down4, up4], axis=3)
+up4 = Conv2D(512, (3, 3), padding='same')(up4)
+up4 = BatchNormalization()(up4)
+up4 = Activation('relu')(up4)
+up4 = Conv2D(512, (3, 3), padding='same')(up4)
+up4 = BatchNormalization()(up4)
+up4 = Activation('relu')(up4)
+up4 = Conv2D(512, (3, 3), padding='same')(up4)
+up4 = BatchNormalization()(up4)
+up4 = Activation('relu')(up4)
+# 16
+
+up3 = UpSampling2D((2, 2))(up4)
+up3 = concatenate([down3, up3], axis=3)
+up3 = Conv2D(256, (3, 3), padding='same')(up3)
+up3 = BatchNormalization()(up3)
+up3 = Activation('relu')(up3)
+up3 = Conv2D(256, (3, 3), padding='same')(up3)
+up3 = BatchNormalization()(up3)
+up3 = Activation('relu')(up3)
+up3 = Conv2D(256, (3, 3), padding='same')(up3)
+up3 = BatchNormalization()(up3)
+up3 = Activation('relu')(up3)
+# 32
+
+up2 = UpSampling2D((2, 2))(up3)
+up2 = concatenate([down2, up2], axis=3)
+up2 = Conv2D(128, (3, 3), padding='same')(up2)
+up2 = BatchNormalization()(up2)
+up2 = Activation('relu')(up2)
+up2 = Conv2D(128, (3, 3), padding='same')(up2)
+up2 = BatchNormalization()(up2)
+up2 = Activation('relu')(up2)
+up2 = Conv2D(128, (3, 3), padding='same')(up2)
+up2 = BatchNormalization()(up2)
+up2 = Activation('relu')(up2)
+# 64
+
+up1 = UpSampling2D((2, 2))(up2)
+up1 = concatenate([down1, up1], axis=3)
+up1 = Conv2D(64, (3, 3), padding='same')(up1)
+up1 = BatchNormalization()(up1)
+up1 = Activation('relu')(up1)
+up1 = Conv2D(64, (3, 3), padding='same')(up1)
+up1 = BatchNormalization()(up1)
+up1 = Activation('relu')(up1)
+up1 = Conv2D(64, (3, 3), padding='same')(up1)
+up1 = BatchNormalization()(up1)
+up1 = Activation('relu')(up1)
+# 128
+
+classify = Conv2D(1, (1, 1), activation='sigmoid')(up1)
+
+model = Model(inputs=inputs, outputs=classify)
+
+
+
+callbacks = [      ModelCheckpoint(monitor='val_loss',
+                             filepath='spatial.{epoch:05d}-L{val_loss:.4f}-A{val_dice_coeff:.4f}.h5',
+                             save_best_only=True,
+                             save_weights_only=True, verbose=1), 
+                   EarlyStopping(monitor='val_loss',
+                             patience=200,
+                             verbose=1,
+                             min_delta=1e-4)]
+
+
+model.compile(optimizer=Nadam(), loss=bce_dice_loss, metrics=[dice_coeff])
+
+xtrain = np.load('spatial_patchwise_xtrain.npy')
+ytrain = np.load('spatial_patchwise_ytrain.npy')
+xval = np.load('spatial_patchwise_xval.npy')
+yval = np.load('spatial_patchwise_yval.npy')
+
+traindatagen = ImageDataGenerator()
+traindatagen.fit(xtrain)
+
+valdatagen = ImageDataGenerator()
+valdatagen.fit(xval)
+
+batch_size = 16
+
+history = model.fit(xtrain, ytrain,
+          batch_size=batch_size,
+          epochs=3000,
+          verbose=1,          
+          validation_data=(xval, yval), callbacks = callbacks)
+
+pickle.dump(history.history, open("History_spatial_patchwise.bin", 'wb'))
+new_model.save("Model_spatial_patchwise.h5")
+model.save("TruModel_spatial_patchwise.h5")
