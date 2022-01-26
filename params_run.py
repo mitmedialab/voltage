f3c9be833c6a3b48865f4b4d53a1cbe3afd56e2c
Yosuke Bando <bandy@media.mit.edu>
2022-01-26 14:46:54 -0500

    increased the patch size for motion correction

--- params_run.py
+++ params_run.py
@@ -5,7 +5,7 @@ PATCH_SHAPE = (64, 64)
 # preprocessing parameters
 MOTION_SEARCH_LEVEL = 2
 MOTION_SEARCH_SIZE = 5
-MOTION_PATCH_SIZE = 10
+MOTION_PATCH_SIZE = 15
 MOTION_PATCH_OFFSET = 7
 SIGNAL_SCALE = 3.0 # must be consistent with training
 
@@ -26,6 +26,6 @@ BATCH_SIZE = 128
 
 INPUT_PATH = '/media/bandy/nvme_data/ramdas/VI/SelectedData_v0.2/WholeTifs'
 GT_PATH = '/media/bandy/nvme_data/ramdas/VI/SelectedData_v0.2/GT_comparison/GTs_rev20201027/consensus'
-PREPROC_PATH = '/media/bandy/nvme_work/voltage/run' #'preproc-db-ms5-sm1-sc0-ss3-sw50'
+PREPROC_PATH = '/media/bandy/nvme_work/voltage/run'
 MODEL_PATH = '/media/bandy/nvme_work/voltage/train/model'
 OUTPUT_PATH = '/media/bandy/nvme_work/voltage/run'
