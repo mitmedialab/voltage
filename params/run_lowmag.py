7dc5c3790d834b84d1cd497c5fa264bd21c8bba3
Yosuke Bando <bandy@media.mit.edu>
2022-03-15 11:16:07 -0400

    added parameter files for reorganized datasets

---
+++ params/run_lowmag.py
@@ -0,0 +1,46 @@
+# common parameters (must be consistent with training)
+TIME_SEGMENT_SIZE = 50
+
+# motion/shading correction parameters
+MOTION_SEARCH_LEVEL = 2
+MOTION_SEARCH_SIZE = 5
+MOTION_PATCH_SIZE = 15
+MOTION_PATCH_OFFSET = 7
+
+# preprocessing parameters
+SIGNAL_SCALE = 3.0
+
+# demixing parameters
+AREA_THRESHOLD = 55
+ACTIVITY_LEVEL_THRESHOLD_RELATIVE = 1/9
+ACTIVITY_LEVEL_THRESHOLD_ABSOLUTE = 0.0001
+
+
+# runtime parameters
+RUN_MODE = 'run' # run the pipeline for neuron detection
+RUN_CORRECT = True
+RUN_PREPROC = True
+RUN_SEGMENT = True
+RUN_DEMIX = True
+RUN_EVALUATE = True
+
+
+# performance parameters (optimal values depend on the computer environment)
+NUM_THREADS_DEMIXING = 16
+GPU_MEM_SIZE = 5 # GB
+
+
+# real data parameters
+TILE_SHAPE = (64, 64)
+TILE_STRIDES = (8, 8)
+BATCH_SIZE = 128
+
+
+# file paths
+import pathlib
+INPUT_DIR = '/media/bandy/nvme_data/voltage/datasets_v0.3/lowmag'
+INPUT_FILES = sorted(pathlib.Path(INPUT_DIR).glob('*.tif'))
+GT_DIR = '/media/bandy/nvme_data/voltage/datasets_v0.3/lowmag_GT_v20201027'
+GT_FILES = [pathlib.Path(GT_DIR).joinpath(f.name) for f in INPUT_FILES]
+MODEL_FILE = '/media/bandy/nvme_work/voltage/models/model20220106_dendrites/model.h5'
+OUTPUT_DIR = '/media/bandy/nvme_work/voltage/lowmag_v0.3'
