08dba977257ac96c31d2f554771d63d83c36bb6a
Yosuke Bando <bandy@media.mit.edu>
2022-11-14 13:26:01 -0500

    added VolPy setup script for comparison

---
+++ compare/volpy/setup_env.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+MODULES="
+tensorflow=1.14.0
+cython
+future
+holoviews
+ipyparallel
+matplotlib
+opencv
+peakutils
+pynwb
+scikit-image
+scikit-learn
+pims
+keras=2.3.1
+tifffile=2020.6.3
+jupyter_client=7.4.2
+h5py=2.10.0
+numpy=1.16.4
+"
+
+for mm in ${MODULES}
+do
+    echo ${mm}
+    conda install -y ${mm} -c conda-forge
+done
+
